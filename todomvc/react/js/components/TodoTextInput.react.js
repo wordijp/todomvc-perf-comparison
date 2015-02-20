ebdc8ac3907a5b330f7bca4a06bc8ecde8e4ce46
Kazuhito Hokamura <k.hokamura@gmail.com>
2015-02-20 16:56:21 +0900

    Update react example

---
+++ todomvc/react/js/components/TodoTextInput.react.js
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2014, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+var React = require('react');
+var ReactPropTypes = React.PropTypes;
+
+var ENTER_KEY_CODE = 13;
+
+var TodoTextInput = React.createClass({
+
+  propTypes: {
+    className: ReactPropTypes.string,
+    id: ReactPropTypes.string,
+    placeholder: ReactPropTypes.string,
+    onSave: ReactPropTypes.func.isRequired,
+    value: ReactPropTypes.string
+  },
+
+  getInitialState: function() {
+    return {
+      value: this.props.value || ''
+    };
+  },
+
+  /**
+   * @return {object}
+   */
+  render: function() /*object*/ {
+    return (
+      <input
+        className={this.props.className}
+        id={this.props.id}
+        placeholder={this.props.placeholder}
+        onKeyDown={this._onKeyDown}
+        autoFocus={true}
+        ref="input"
+      />
+    );
+  },
+
+  /**
+   * Invokes the callback passed in as onSave, allowing this component to be
+   * used in different ways.
+   */
+  _save: function() {
+    this.props.onSave(this.refs.input.getDOMNode().value);
+    this.refs.input.getDOMNode().value = '';
+  },
+
+  /**
+   * @param  {object} event
+   */
+  _onKeyDown: function(event) {
+    if (event.nativeEvent.which === ENTER_KEY_CODE) {
+      this._save();
+    }
+  }
+
+});
+
+module.exports = TodoTextInput;
